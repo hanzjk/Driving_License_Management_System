d6f2bf9f6e1be4493c2b452fcadedd5d1f0db9ec
Hansi Karunarathna <65526190+hanzjk@users.noreply.github.com>
2022-05-17 13:50:48 +0530

    Update functions.inc.php

--- Driving_School/includes/functions.inc.php
+++ Driving_School/includes/functions.inc.php
@@ -196,8 +196,8 @@ function sendEmail($email, $body, $subject)
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'dlmslk2021@gmail.com';                     // SMTP username
-        $mail->Password   = 'DLMS2021';                               // SMTP password
+        $mail->Username   = '';                     // SMTP username
+        $mail->Password   = '';                               // SMTP password
         $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
